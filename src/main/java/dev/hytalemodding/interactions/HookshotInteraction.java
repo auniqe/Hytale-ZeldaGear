e6b6cdb995f1a2e8b409d8dbc6ec8f09dcfb0290
paintedgirl <139597851+auniqe@users.noreply.github.com>
2026-01-19 22:23:22 -0800

    Hookshot Implemented

--- src/main/java/dev/hytalemodding/interactions/HookshotInteraction.java
+++ src/main/java/dev/hytalemodding/interactions/HookshotInteraction.java
@@ -3,25 +3,20 @@ package dev.hytalemodding.interactions;
 import com.hypixel.hytale.codec.builder.BuilderCodec;
 import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Store;
-import com.hypixel.hytale.math.vector.Transform;
 import com.hypixel.hytale.math.vector.Vector3d;
-import com.hypixel.hytale.math.vector.Vector3f;
 import com.hypixel.hytale.protocol.*;
+import com.hypixel.hytale.server.core.entity.InteractionContext;
 import com.hypixel.hytale.server.core.entity.entities.Player;
-import com.hypixel.hytale.server.core.modules.entity.component.EntityScaleComponent;
-import com.hypixel.hytale.server.core.modules.entity.component.HeadRotation;
+import com.hypixel.hytale.server.core.meta.MetaKey;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
-import com.hypixel.hytale.server.core.modules.entity.player.PlayerSkinComponent;
-import com.hypixel.hytale.server.core.modules.interaction.interaction.config.selector.RaycastSelector;
+import com.hypixel.hytale.server.core.modules.interaction.interaction.CooldownHandler;
 import com.hypixel.hytale.server.core.modules.physics.component.Velocity;
-import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.core.util.TargetUtil;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.config.SimpleInteraction;
 
 import javax.annotation.Nonnull;
-
 
 public class HookshotInteraction extends SimpleInteraction {
 
@@ -32,87 +27,79 @@ public class HookshotInteraction extends SimpleInteraction {
     private final double pullSpeed = 25; //blocks per second
 
     public static final String INTERACTION_NAME = "HookshotInteraction";
-    public Vector3d target = new Vector3d(0,90,0);
+
+    //set up this functions data
+    public static final MetaKey<Vector3d> HIT_LOCATION = CONTEXT_META_REGISTRY.registerMetaObject(data -> null);
 
     @Override
     protected void tick0(
         boolean firstRun,
         float time,
-        @Nonnull com.hypixel.hytale.protocol.InteractionType type,
-        @Nonnull com.hypixel.hytale.server.core.entity.InteractionContext context,
-        @Nonnull com.hypixel.hytale.server.core.modules.interaction.interaction.CooldownHandler cooldownHandler
+        @Nonnull InteractionType type,
+        @Nonnull InteractionContext context,
+        @Nonnull CooldownHandler cooldownHandler
     ) {
+        float predicted_time_to_target = 1.5f;
+
+        //prefunction setup
         CommandBuffer<EntityStore> commandBuffer = context.getCommandBuffer();
         Player player = commandBuffer.getComponent(context.getEntity(), Player.getComponentType());
+        Ref<EntityStore> ref = player.getReference();
+        Store<EntityStore> playerStore = player.getReference().getStore();
         InteractionSyncData contextState = context.getState();
+
+        //initial setup
         if (firstRun) {
-            Store<EntityStore> playerStore = player.getReference().getStore();
-            Vector3d EyeDir = playerStore.getComponent(player.getReference(), com.hypixel.hytale.server.core.modules.entity.component.HeadRotation.getComponentType()).getDirection();
-            Vector3d EyePos = playerStore.getComponent(player.getReference(), TransformComponent.getComponentType()).getPosition().add();
-            player.getReference().getStore().getComponent(player.getReference(), com.hypixel.hytale.server.core.modules.entity.component.PositionDataComponent.getComponentType()).
+            //raycast
+            Vector3d hitLocation = TargetUtil.getTargetLocation(player.getReference(), blockId -> blockId != 0, 30.0, playerStore);
 
+            //cancel if not hit
+            if(hitLocation == null) return;
 
-            contextState.state = InteractionState.NotFinished;
-            // Raycast
-            Ref<EntityStore> ref = context.getEntity();
-            Store<EntityStore> store = ref.getStore();
-            World world = store.getExternalData().getWorld();
-
-            RaycastSelector r = new RaycastSelector();
-            r.newSelector().selectTargetBlocks();
-
-            Transform look = TargetUtil.getLook(ref, context.getCommandBuffer());
-            System.out.println(look);
-            Vector3d origin = look.getPosition();
-            System.out.println(origin);
-            Vector3d direction = look.getDirection();
-            System.out.println(direction);
-
-            target = origin.add(direction.normalize().scale(30)); // raycast 10 blocks
-            System.out.println(target);
+            //set data
+            context.getMetaStore().putMetaObject(HIT_LOCATION, hitLocation);
         }
 
-        player.getReference().getStore().getComponent()
+        //handle pulling
+        contextState.state = InteractionState.NotFinished;
+        Vector3d target = context.getMetaStore().getMetaObject(HIT_LOCATION);
+        pullPlayer(player, target);
 
-        if (!firstRun) {
-            Ref<EntityStore> ref = context.getEntity();
-            pullPlayer(ref, ref.getStore(), target, ref.getStore().getExternalData().getWorld(), player, contextState);
-
-            contextState.state = InteractionState.NotFinished;
-            System.out.println(target);
+        //end when at target or if predicted time was run out
+        Vector3d current = playerStore.getComponent(ref, TransformComponent.getComponentType()).getPosition();
+        double distance = target.distanceTo(current);
+        if (distance < 1.5 || time > predicted_time_to_target) {
+            contextState.state = InteractionState.Finished;
         }
     }
 
-    private void pullPlayer(Ref<EntityStore> ref, Store<EntityStore> store, Vector3d target, World world, Player player, InteractionSyncData contextState) {
-        System.out.println("pullplayer called");
-        world.execute(() -> {
-            Vector3f playerRot = player.getReference().getStore().getComponent(player.getReference(), com.hypixel.hytale.server.core.modules.entity.component.HeadRotation.getComponentType()).getRotation();
+    private void pullPlayer(Player player, Vector3d target) {
+        if(player.getReference() == null) return;
+        if(player.getWorld() == null) return;
 
-            System.out.println("INSIDE CHUNK THREAD!");
+        //run on world thread to modify player
+        player.getWorld().execute(() -> {
+            Store<EntityStore> store = player.getReference().getStore();
+
             // Get player transform
-            var transform = store.getComponent(ref, com.hypixel.hytale.server.core.modules.entity.component.TransformComponent.getComponentType());
+            var transform = store.getComponent(player.getReference(), TransformComponent.getComponentType());
             if (transform == null) return;
-            System.out.println("transform was NOT NULL");
 
+            //calculate difference between position and destination
             Vector3d current = transform.getPosition();
-            Vector3d toTarget = target.subtract(current);
+            Vector3d toTarget = target.clone().subtract(current);
             double distance = toTarget.length();
+            Vector3d step = toTarget.normalize().scale(pullSpeed);
 
-            if (distance < 1) {
-                contextState.state = InteractionState.Finished;
-                return;
+            //stop player at destination (avoids launching them off the sides)
+            if (distance < 1.8) {
+                step = new Vector3d(0,0,0);
             }
-            System.out.println("distance not reached");
 
-            Vector3d step = toTarget.normalize().scale(Math.min(pullSpeed, distance));
-
-            if (player.getReference() == null) return;
-
-            Store<EntityStore> store2 = player.getReference().getStore();
-
-
-            Velocity velo = store2.getComponent(player.getReference(), Velocity.getComponentType());
+            //set velocity
+            Velocity velo = store.getComponent(player.getReference(), Velocity.getComponentType());
             velo.addInstruction(step, null, ChangeVelocityType.Set);
+
         });
     }
 }
